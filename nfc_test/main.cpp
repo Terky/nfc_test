9118fa8dc344452f1a0e10e7fa3253daaaacb07c
Terky <artemab@outlook.com>
2017-09-22 17:29:06 +0300

    Initial Commit

---
+++ nfc_test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  nfc_test
+//
+//  Created by Артём Бурмистров on 22/09/2017.
+//  Copyright © 2017 Артём Бурмистров. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
