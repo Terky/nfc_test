0c526db20b6b4e10820e8492de38a1efd7fbdbd5
Terky <artemab@outlook.com>
2019-10-26 18:50:40 +0300

    Updated code description

--- nfc_test/main.cpp
+++ nfc_test/main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <ctime>
 
-// Только для macOS, для остальных надо искать PCSC
-// Для Linux точно есть PCSC-lite
 #include <PCSC/wintypes.h>
 #include <PCSC/winscard.h>
 
@@ -15,33 +13,34 @@ void print_bytes(BYTE * bytes, BYTE bytes_size)
 }
 
 int main() {
-    // По идее должен быть тип LONG (если следовать документации PCSC)
-    // Но в macOS функция возвращает этот тип
-    // Этого же типа константы, которые позволяют определить ошибки
+    // This variavle should have type LONG (according to PCSC documentation)
+    // But in macOS it have this type for some reason and it is longer that LONG
+    // Constants, that are defining errors, have the same type too
     uint32_t rv;
 
     SCARDCONTEXT hContext;
     SCARDHANDLE hCard;
     DWORD dwActiveProtocol;
 
-    // Соединение с картой
+    // Connecting to the card
     rv = SCardEstablishContext(SCARD_SCOPE_SYSTEM, NULL, NULL, &hContext);
     if (rv == SCARD_S_SUCCESS) {
         time_t t = clock();
+        // Hardcoded for specific NFC-reader that I have at the moment
         rv = SCardConnect(hContext, "ACS ACR122U PICC Interface", SCARD_SHARE_SHARED,
                           SCARD_PROTOCOL_T0 | SCARD_PROTOCOL_T1, &hCard, &dwActiveProtocol);
         while (rv != SCARD_S_SUCCESS) {
             rv = SCardConnect(hContext, "ACS ACR122U PICC Interface", SCARD_SHARE_SHARED,
                               SCARD_PROTOCOL_T0 | SCARD_PROTOCOL_T1, &hCard, &dwActiveProtocol);
             double dif = double(clock() - t) / CLOCKS_PER_SEC;
-            // Таймер
+            // Timer
             if (dif > 1) {
                 std::cout << "Timeout reached" << std::endl;
                 return 1;
             }
         }
 
-        if (rv == SCARD_S_SUCCESS) { // Выбор протокола
+        if (rv == SCARD_S_SUCCESS) { // Protocol choosing
             SCARD_IO_REQUEST* request;
             switch (dwActiveProtocol) {
                 case SCARD_PROTOCOL_T1:
@@ -53,7 +52,7 @@ int main() {
                 default:
                     request = nullptr;
                     break;
-            } // Выбор протокола
+            } // Protocol choosing
 
             // Get UID for ISO
             //BYTE sendBuf[] = { 0x00, 0xA4, 0x04, 0x00, 0x05, 0xF2, 0x22, 0x22, 0x22, 0x22 };
@@ -69,14 +68,13 @@ int main() {
             */
 
 
-            { // Загрузка ключа А
+            { // Uploading key А
                 BYTE loadKeyRes[2];
                 DWORD recvBufLength = sizeof(loadKeyRes);
-                // После смены ключа, для аутентификации необходимо использовать
-                // запись ключа в том же порядке, что и при записи
-                BYTE loadKeyCmd[] = { /* Команда */ 0xFF, 0x82, 0x00, 0x00,
-                                      /* Длина ключа */ 0x06,
-                                      /* Ключ */ 0xBB, 0xBB, 0xBB, 0xBB, 0xBB, 0xBC };
+                // After key changing we need to use the same writing order as it was in reading
+                BYTE loadKeyCmd[] = { /* Command */ 0xFF, 0x82, 0x00, 0x00,
+                                      /* Key length */ 0x06,
+                                      /* Key value */ 0xBB, 0xBB, 0xBB, 0xBB, 0xBB, 0xBC };
                 DWORD sendBufLength = sizeof(loadKeyCmd);
                 rv = SCardTransmit(hCard, request, loadKeyCmd, sendBufLength, NULL, loadKeyRes, &recvBufLength);
 
@@ -84,16 +82,16 @@ int main() {
                     std::cout << "Load key success" << std::endl;
                 else
                     std::cout << "Load key error" << std::endl;
-            } // Загрузка ключа А
+            } // Uploading key А
 
-            { // Аутентификация сектора
+            { // Sector auth
                 BYTE generalAuthRes[2];
                 DWORD recvBufLength = sizeof(generalAuthRes);
-                // В типе ключа 1 байт = 0x60, если выбран ключ А и 0x61, если ключ В
+                // Key type should have first byte = 0x60 in case of using А key and 0x61 in case of В key
                 BYTE generalAuthCmd[] = {
-                    /* Команда на аутентификацию */ 0xFF, 0x86, 0x00, 0x00, 0x05, 0x01, 0x00,
-                    /* Адрес блока */ 0x13,
-                    /* Тип ключа */ 0x61, 0x00
+                    /* Auth command */ 0xFF, 0x86, 0x00, 0x00, 0x05, 0x01, 0x00,
+                    /* Block address */ 0x13,
+                    /* Key type */ 0x61, 0x00
                 };
                 DWORD sendBufLength = sizeof(generalAuthCmd);
 
@@ -103,10 +101,10 @@ int main() {
                     std::cout << "Authenticate success" << std::endl;
                 else
                     std::cout << "Authenticate error" << std::endl;
-            } // Аутентификация сектора
+            } // Sector auth
 
-            { // Чтение сектора
-                // Записываю нули чтобы было проще отслеживать ошибки
+            { // Reading a sector
+                // Initializing with zeros for easier error handling
                 BYTE readBlockRes[18] = {
                     0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
                     0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
@@ -114,16 +112,16 @@ int main() {
                 };
                 DWORD recvBufLength = sizeof(readBlockRes);
                 BYTE readBlockCmd[] = {
-                    /* Команда */ 0xFF, 0xB0, 0x00,
-                    /* Адрес блока */ 0x13,
-                    /* Ожидаемая длина ответа */ 0x10
+                    /* Command */ 0xFF, 0xB0, 0x00,
+                    /* BLock address */ 0x13,
+                    /* Expected response length */ 0x10
                 };
-                // В ожидаемой длине ответа не учитываются 2 байта SW1 и SW2, указывающие на ошибки при обработке команд
-                // При этом длина буфера ответа должна учитывать эти байты, иначе возникает ошибка SCARD_E_INSUFFICIENT_BUFFER
+                // In response 2 bytes should be ingored: SW1 and SW2, that are used for error indication
+                // In any case the buffer should have that 2 redundant bytes, otherwise the SCARD_E_INSUFFICIENT_BUFFER error occures
                 DWORD sendBufLength = sizeof(readBlockCmd);
                 rv = SCardTransmit(hCard, request, readBlockCmd, sendBufLength, NULL, readBlockRes, &recvBufLength);
 
-                // Проверка ошибок чтения сектора
+                // Error handeling
                 if (rv == SCARD_S_SUCCESS) {
                     //unsigned int* i = (unsigned int*)UID;
                     std::cout << "Success"/* << *i*/ << std::endl;
@@ -169,29 +167,29 @@ int main() {
                     }
                 std::cout << "Transmission error" << std::endl;
                 return 1;
-            } // Проверка ошибок чтения сектора
-        } // Чтение сектора
+            } // Error handeling
+        } // Reading a sector
 
-            { // Запись сектор
+            { // Writing to a sector
                 BYTE writeBlockRes[2];
                 DWORD recvBufLength = sizeof(writeBlockRes);
                 BYTE writeBlockCmd[] = {
-                    /* Команда */ 0xFF, 0xD6, 0x00,
-                    /* Адрес блока */ 0x13,
-                    /* Длина нового блока (блоков) */ 0x10,
-                    /* Блок (блоки) на запись */
-                        /* Ключ А */ 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA,
-                        /* Биты доступа и дополнительня информация */ 0xFF, 0x07, 0x80, 0x69,
-                        /* Ключ В */ 0xBB, 0xBB, 0xBB, 0xBB, 0xBB, 0xBC
+                    /* Command */ 0xFF, 0xD6, 0x00,
+                    /* Block address */ 0x13,
+                    /* New block (blocks) size */ 0x10,
+                    /* Blocks (block) to write */
+                        /* Key A */ 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA,
+                        /* Access bits and auxillary info */ 0xFF, 0x07, 0x80, 0x69,
+                        /* Key B */ 0xBB, 0xBB, 0xBB, 0xBB, 0xBB, 0xBC
                 };
-                // Длина блока должна быть равна 0x10 или более, если требуется запись сразу нескольких блоков
-                // Запись нескольких блоков работает только для блоков с данными, но не для блоков с ключами
+                // Block size should be 0x10 or more, if reqiured to write more than one block
+                // Multiple block writing works. with data block only, not with key block
                 DWORD sendBufLength = sizeof(writeBlockCmd);
                 //rv = SCardTransmit(hCard, request, writeBlockCmd, sendBufLength, NULL, writeBlockRes, &recvBufLength);
                 print_bytes(writeBlockRes, recvBufLength);
-        } // Запись сектора
+        } // Writing to a sector
 
-        } else { // Проверка ошибок соединения с картой
+        } else { // Handking card connection error
             std::cout << rv << " Card connection error" << std::endl;
             return 1;
         }
@@ -199,7 +197,7 @@ int main() {
     } else {
         std::cout << "Establishing connection error" << std::endl;
         return 1;
-    } // Соединение с картой
+    } // Connecting to the card
     SCardReleaseContext(hContext);
     
     return 0;
